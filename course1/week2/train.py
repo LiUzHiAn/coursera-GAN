e31928045dea4a289a3ca989e3a8319e032c14e4
LiuZhian <csliuzhian@mail.scut.edu.cn>
2020-10-19 19:34:24 +0800

    add course1-week2

---
+++ course1/week2/train.py
@@ -0,0 +1,104 @@
+import os
+import torch.nn as nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST  # Training dataset
+from course1.week2.models import Generator, Discriminator
+from course1.week1.utils import *
+
+torch.manual_seed(2020)
+
+
+# initialize the weights to the normal distribution
+# with mean 0 and standard deviation 0.02
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+
+
+criterion = nn.BCEWithLogitsLoss()
+z_dim = 64
+display_step = 2000
+batch_size = 128
+# A learning rate of 0.0002 works well on DCGAN
+lr = 2e-4
+
+# These parameters control the optimizer's momentum, which you can read more about here:
+# https://distill.pub/2017/momentum/ but you don’t need to worry about it for this course!
+beta_1 = 0.5
+beta_2 = 0.999
+device = 'cuda:0'
+
+# You can tranform the image values to be between -1 and 1 (the range of the tanh activation)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+# Load MNIST dataset as tensors
+dataset_root = "/home/liuzhian/hdd/datasets/MNIST"
+# dataset_root = "/Users/liuzhian/common_datasets/MNIST"
+dataloader = DataLoader(
+    MNIST(root=dataset_root, download=True, transform=transform),
+    batch_size=batch_size,
+    shuffle=True)
+
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+disc = Discriminator().to(device)
+disc_opt = torch.optim.Adam(disc.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+gen = gen.apply(weights_init)
+disc = disc.apply(weights_init)
+
+n_epochs = 50
+cur_step = 0
+mean_generator_loss = 0
+mean_discriminator_loss = 0
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for real, _ in tqdm(dataloader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        ## Update discriminator ##
+        disc_opt.zero_grad()
+        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        fake = gen(fake_noise)
+        disc_fake_pred = disc(fake.detach())
+        disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_pred = disc(real)
+        disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+
+        # Keep track of the average discriminator loss
+        mean_discriminator_loss += disc_loss.item() / display_step
+        # Update gradients
+        disc_loss.backward(retain_graph=True)
+        # Update optimizer
+        disc_opt.step()
+
+        ## Update generator ##
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        disc_fake_pred = disc(fake_2)
+        gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+        gen_loss.backward()
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        mean_generator_loss += gen_loss.item() / display_step
+
+        ## Visualization code ##
+        if cur_step % display_step == 0 and cur_step > 0:
+            print(
+                f"Step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+            show_tensor_images(fake, fig_save_name="./vis/%d_fake.png" % cur_step)
+            show_tensor_images(real, fig_save_name="./vis/%d_real.png" % cur_step)
+            mean_generator_loss = 0
+            mean_discriminator_loss = 0
+        cur_step += 1
